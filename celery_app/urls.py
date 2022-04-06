45603e8ec1084578e69163f339afd5796121a4d4
SaberHardy <samirsamado5@gmail.com>
2022-04-06 19:19:24 +0300

    send email in specific time

--- celery_app/urls.py
+++ celery_app/urls.py
@@ -4,4 +4,5 @@ from celery_app import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('send_mail/', views.send_email_to_all, name='send_mail'),
+    path('program_mail/', views.send_mail_at_particular_time, name='program_mail'),
 ]
--- celery_app/views.py
+++ celery_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from send_email.tasks import send_mail_func
 from celery_app.tasks import test_func
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
+import json
 
 
 def home(request):
@@ -13,3 +15,13 @@ def home(request):
 def send_email_to_all(request):
     send_mail_func.delay()
     return HttpResponse("Successfully sent")
+
+
+def send_mail_at_particular_time(request):
+    schedule, created = CrontabSchedule.objects.get_or_create(hour=19, minute=16)
+    task = PeriodicTask.objects.create(crontab=schedule,
+                                       name='schedule_mail_task_' + '5',
+                                       task='send_email.tasks.send_mail_func',
+                                       # args=json.dumps(([2, 3]))
+                                       )
+    return HttpResponse("send_mail_at_particular_time Successfully sent")
