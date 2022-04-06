13e8a1fb7c69d592c8d13cc7d8e85baf4bece013
SaberHardy <samirsamado5@gmail.com>
2022-04-06 18:30:05 +0300

    schedule email using worker & beat

--- celery_dimo/celery.py
+++ celery_dimo/celery.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
-
+from celery.schedules import crontab
 from django.conf import settings
 import os
+
+from send_email.tasks import send_mail_func
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_dimo.settings')
 
@@ -14,7 +16,14 @@ app.conf.update(timezone='Europe/Moscow')
 app.config_from_object(settings, namespace='CELERY')
 
 app.conf.beat_schedule = {
-
+    # Task name
+    'send-email-everyday-at-8am': {
+        'task': 'send_email.tasks.send_mail_func',
+        'schedule': crontab(hour=18, minute=23),
+        # this args we will pass here we can use them
+        # in our function 'send_mail_func' as inputs
+        # 'args': (2, )
+    }
 }
 
 # Load task modules from all registered Django app configs.
